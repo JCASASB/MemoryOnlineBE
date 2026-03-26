f88d91a543d274892fb11c3ecbf3029115a23bc3
ginnkp <ginn1234@gmail.com>
2026-03-26 12:54:40 +0100

    fix ignored file

--- src/01.Apis/MemoryOnline.Apis.Utils/DTOs/Out/GameStateDtoOut.cs
+++ src/01.Apis/MemoryOnline.Apis.Utils/DTOs/Out/GameStateDtoOut.cs
@@ -6,5 +6,11 @@ namespace MemoryOnline.Apis.Utils.DTOs.Out
 {
     public class GameStateDtoOut
     {
+        public string? id { get; set; }
+        public string? name { get; set; }
+        public List<CardDtoOut>? cards { get; set; }
+        public bool isProcessing { get; set; }
+        public int level { get; set; }
+        public List<PlayerDtoOut>? players { get; set; }
     }
 }
