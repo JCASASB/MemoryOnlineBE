00f9cb506e60acd3c9521334e08b11f08c4e7e18
ginnkp <ginn1234@gmail.com>
2026-03-26 12:08:32 +0100

    try fix pipeline

---
+++ src/01.Apis/MemoryOnline.Apis.Utils/DTOs/In/CardDtoIn.cs
@@ -0,0 +1,11 @@
+namespace MemoryOnline.Apis.Utils.DTOs.In
+{
+    public class CardDtoIn
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+        public string ImgUrl { get; set; }
+        public bool IsRevealed { get; set; }
+        public bool IsMatched { get; set; }
+    }
+}
